c4ea4710e9534eab9a3d6f13463cccefdc284f6d
Dr.Zenith <professorazmuth@gmail.com>
2024-12-29 13:16:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,39 +15,36 @@ cat ./assets/logo.txt
 printf "${RESET}\n"
 
 # GitHub information
-printf "${YELLOW}GitHub Repository:${RESET} https://github.com/V0iceX-Anonymizer.git\n"
-printf "${MAGENTA}Author:${RESET} Dr.Zenith\n\n"
+printf "${YELLOW}GitHub Repository:${RESET} https://github.com/azmuthX/citizen39.git\n"
+printf "${MAGENTA}Author:${RESET} Dr. AZ \n\n"
+
+# Step1: Configure network(Tor+VPN)
+printf "${CYAN} Configuring network ...${RESET}\n"
+bash ./script/network_setup.sh
+
+# Step2: Setting up hotspot(optional)
+printf "${YELLOW} Setting up hotspot ...${RESET}\n"
+bash ./script/hotspot.sh
 
 # Menu-driven setup
 while true; do
-    printf "\n${CYAN}Select an option:${RESET}\n"
-    printf "1. Configure network (VPN + Tor)\n"
-    printf "2. Set up hotspot (optional)\n"
-    printf "3. Dial a call with voice modulation\n"
-    printf "4. View logs\n"
-    printf "5. Exit\n"
+    printf " #1 Make a Call\n"
+    printf " #2 View logs\n"
+    printf " #3 EXIT\n"
     printf "${GREEN}Enter your choice: ${RESET}"
     read CHOICE
 
     case $CHOICE in
         1)
-            printf "${CYAN}Configuring network...${RESET}\n"
-            bash ./script/network_setup.sh
-            ;;
-        2)
-            printf "${CYAN}Setting up hotspot...${RESET}\n"
-            bash ./script/hotspot.sh
-            ;;
-        3)
             printf "${CYAN}Starting voice modulation and dialing...${RESET}\n"
             bash ./script/voice_mod.sh &
             bash ./script/voip_call.sh
             ;;
-        4)
+        2)
             printf "${CYAN}Displaying logs:${RESET}\n"
             cat ./logs/v0icex.log
             ;;
-        5)
+        3)
             printf "${RED}Cleaning up logs...${RESET}\n"
             rm -f ./logs/v0icex.log
             printf "${CYAN}Deleting logs successfull... Now Exiting...${RESET}\n"
